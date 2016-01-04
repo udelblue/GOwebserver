102a18ae75de97662cf8f551347cc6eb82fb5277
udelblue <cdsommers@gmail.com>
2016-01-04 11:35:40 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import  (
+	"fmt"
+	"io/ioutil"
+	"io"
+	"net/http"
+	"strings"
+)
+
+
+type Page struct{
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+
+    body, err := ioutil.ReadFile( filename)
+	
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+
+}
+
+
+
+
+
+//http.Dir(".")
+
+
+func processinghttp(w http.ResponseWriter, r *http.Request) {
+	
+
+	p, _ := loadPage(strings.TrimLeft( r.URL.Path , "/"))
+	
+	if p != nil {
+		io.WriteString(w, "Content: " + string(p.Body) + "\n")
+	} else {
+			//404 page not found
+			
+		  io.WriteString(w, string("  ") +"\n")	
+
+		  
+	}
+
+	
+	
+}
+
+
+
+func main() {
+  
+	fmt.Println("starting server ...")
+	http.HandleFunc("/", processinghttp)
+	http.ListenAndServe(":8080", nil)
+	
+	
+}
